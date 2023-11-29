eba3fbe7ee87217d8c1bd9dc833b2a858ae6d15e
chae-young <cycy8527@gmail.com>
2023-11-29 13:08:50 +0900

    text: 미들웨어 작동중..

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest, response: NextResponse) {
   const session = request.cookies.get('session')
-  console.log('미들웨어')
+  console.log('미들웨어 작동중...')
   //세션 없으면 로그인으로 리다이렉트
   if (!session) {
     return NextResponse.redirect(new URL('/login', request.url))
