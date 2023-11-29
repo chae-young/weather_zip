4ff6a8f5ba67eee8751c8f2e22519c3ed1c5314a
chae-young <cycy8527@gmail.com>
2023-11-29 13:16:38 +0900

    Feat: 비슷한 온도 옷차림

---
+++ src/app/home/_components/TempClothingList.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from 'next/image'
+import SkeletonRecommendList from '../_skeleton/SkeletonRecommendList'
+import {
+  ItempClothing,
+  useGetTempClothingList,
+} from '@/hooks/swr/useGetTempClothingList'
+
+interface TempClothingProps {
+  isLogged: boolean
+}
+const TempClothing = ({ isLogged }: TempClothingProps) => {
+  const { tempClothingList, isValidating } = useGetTempClothingList(isLogged)
+  if (isValidating) {
+    return <SkeletonRecommendList />
+  }
+
+  return (
+    <section className="mt-12">
+      <h3 className="text-h4 mb-3">내가 비슷한 온도에 입었어요.</h3>
+      <div className="flex overflow-x-auto space-x-8 mb-6">
+        <div className="flex shrink-0 [&>*:last-child]:mr-0">
+          {tempClothingList?.map((list: ItempClothing) => (
+            <div className="rounded-2xl overflow-hidden mr-1" key={list.id}>
+              <Image
+                src={list.fullbody_image}
+                alt=""
+                width={140}
+                height={180}
+                className="object-cover h-[180px] w-[140px]"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default TempClothing
