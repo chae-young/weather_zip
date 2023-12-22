e91f6c77444ea10c121c43291ba479104230f666
chae-young <cycy8527@gmail.com>
2023-12-22 13:49:56 +0900

    Fix: 배포시 로그인으로 바로 리다이렉트 되는 현상

--- src/app/user/mypage/page.tsx
+++ src/app/user/mypage/page.tsx
@@ -3,11 +3,8 @@ import InnerCon from '@/app/_components/common/InnerCon'
 import Nav from '@/app/_components/common/Nav'
 import CategoryList from './_components/CategoryList'
 import fetchUser from '../fetchUser'
-import CollectionLengInfo from './_components/CollectionLengInfo'
 
 const Mypage = async () => {
-  const user = await fetchUser()
-
   return (
     <>
       <TopTitle title="마이페이지" />
@@ -16,8 +13,6 @@ const Mypage = async () => {
           <div className="w-[100px] h-[100px] rounded-full bg-pointColor flex justify-center items-center text-gray-400">
             준비중...
           </div>
-          <h2 className="text-2xl mt-4">{user?.nickname}</h2>
-          <CollectionLengInfo uid={user.uid} />
         </section>
         <section>
           <CategoryList />
