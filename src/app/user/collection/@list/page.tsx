98f9531f448425c04ed35f193c2ff78276ce75e6
chae-young <cycy8527@gmail.com>
2024-01-29 19:21:44 +0900

    Remove: 필요없는 import

--- src/app/user/collection/@list/page.tsx
+++ src/app/user/collection/@list/page.tsx
@@ -1,9 +1,6 @@
-import { useRecoilState, useRecoilValue } from 'recoil'
 import fetchUser from '../../fetchUser'
 import CollectionList from '../_components/CollectionList'
 import { fetchCollection } from '../fetchCollection'
-import userAtom from '@/recoil/atom/userAtom'
-import useGetCollection from '@/hooks/swr/useGetCollection'
 
 const Collection = async ({
   searchParams,
@@ -14,8 +11,7 @@ const Collection = async ({
   const tempMin = Number(searchParams.temp_min)
   const tempMax = Number(searchParams.temp_max)
   const userInfo = await fetchUser()
-  console.log(userInfo)
-  //const { collections } = useGetCollection(tempMin, tempMax)
+
   const collections = await fetchCollection({
     tempMin: tempMin,
     tempMax: tempMax,
