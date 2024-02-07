6bb5881ff4383ac0c8b4895714749d7ec79b8375
chae-young <cycy8527@gmail.com>
2024-02-07 14:54:07 +0900

    Fix: 로그인 실패시 파베에러로 분기처리

--- src/app/user/fetchUser.ts
+++ src/app/user/fetchUser.ts
@@ -1,9 +1,10 @@
 import { collection, getDocs, query, where } from 'firebase/firestore'
-import { cookies } from 'next/headers'
+import { cookies, headers } from 'next/headers'
 import { db } from '../../../firebase/firebasedb'
 import { auth } from 'firebase-admin'
 import { adminInitApp } from '../../../firebase/firebase-admin-config'
 import { redirect } from 'next/navigation'
+import { FirebaseError } from 'firebase/app'
 
 export type Tuser = {
   isLogged: boolean
@@ -17,22 +18,37 @@ const fetchUser = async (): Promise<Tuser | null> => {
   const idToken = cookieStore.get('session')?.value
   if (!idToken) return null
 
-  const decodedClaims = await auth().verifySessionCookie(idToken!, false)
-  if (!decodedClaims.uid) redirect('/login')
+  try {
+    const decodedClaims = await auth().verifySessionCookie(idToken!, false)
+    if (!decodedClaims.uid) redirect('/login')
 
-  //실시간 유저 업데이트시 정보 제공
-  const q = query(
-    collection(db, 'users'),
-    where('uid', '==', decodedClaims.uid),
-  )
-  const querySnapshot = await getDocs(q)
-  const userInfo = querySnapshot.docs.map((doc: any) => ({
-    ...doc.data(),
-    isLogged: true,
-    nickname: doc.data().nickname,
-  }))[0]
+    //실시간 유저 업데이트시 정보 제공
+    const q = query(
+      collection(db, 'users'),
+      where('uid', '==', decodedClaims.uid),
+    )
+    const querySnapshot = await getDocs(q)
+    const userInfo = querySnapshot.docs.map((doc: any) => ({
+      ...doc.data(),
+      isLogged: true,
+      nickname: doc.data().nickname,
+    }))[0]
 
-  return userInfo
+    return userInfo
+  } catch (error) {
+    const firebaseError = error as FirebaseError
+    if (firebaseError) {
+      const headersList = headers()
+      const pathname = headersList.get('next-url')
+
+      if (pathname) redirect('/login')
+      return { isLogged: false, uid: '', nickname: '', email: '' }
+    } else {
+      // FirebaseError가 아닌 다른 에러 처리
+      console.error('Non-Firebase Error:', error)
+      throw new Error('Non-Firebase Error')
+    }
+  }
   // const cookieStore = cookies()
   // const idToken = cookieStore.get('session')?.value
 
