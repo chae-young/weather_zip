7bd443926dd1eb160b721daf4ae4dc581c6717a6
chae-young <cycy8527@gmail.com>
2024-02-13 13:57:24 +0900

    Fix: 다시 재수정...
    - <CurrentDaysWeather />가 없어지는 이슈가 발생

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,12 @@
 import dynamic from 'next/dynamic'
+import fetchUser from '@/app/user/fetchUser'
+import Nav from '../_components/common/Nav'
+const RecommendList = dynamic(() => import('./_components/RecommendList'), {
+  ssr: false,
+})
+const TempClothing = dynamic(() => import('./_components/TempClothingList'), {
+  ssr: false,
+})
 
 const CurrentWeather = dynamic(() => import('./_components/currentWeather'), {
   ssr: false,
@@ -11,10 +19,22 @@ const CurrentDaysWeather = dynamic(
 )
 
 const Home = async () => {
+  const user = await fetchUser()
   return (
     <>
-      <CurrentWeather />
-      <CurrentDaysWeather />
+      <section className="bg-pointBg flex flex-col justify-center items-center relative overflow-hidden">
+        <CurrentWeather />
+        <CurrentDaysWeather />
+      </section>
+      <article className="w-full bg-pointBg">
+        <section className="w-full rounded-tl-[30px] rounded-tr-[30px] bg-white pt-10 px-5 min-h-min pb-16">
+          <RecommendList />
+          {user?.isLogged && (
+            <TempClothing isLogged={user?.isLogged} uid={user.uid} />
+          )}
+        </section>
+        <Nav />
+      </article>
     </>
   )
 }
